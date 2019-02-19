85646b1fc82df1b85adefb3ec14cf4b4b5ae0a5a
Joe Netti <jnetti@blockstream.io>
2019-02-19 11:45:40 -0800

    added functionality to create fake gossip channel updates.

--- src/gossip.py
+++ src/gossip.py
@@ -3,6 +3,7 @@ import pickle
 from config import *
 from common.networkClasses import *
 from random import randint
+import common.utility as utility
 import common.wif as wif
 from bitcoin.wallet import CBitcoinSecret, P2PKHBitcoinAddress
 from bitcoin import SelectParams
@@ -10,15 +11,34 @@ import hashlib
 from bitcoin.signmessage import BitcoinMessage, VerifyMessage, SignMessage
 import base64
 import sys
+import time
 
 chainHash = bytearray.fromhex('06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f')
-announcementType = bytearray().fromhex("0100")
+channelType = bytearray().fromhex("0100") #256
+updateType = bytearray().fromhex("0102") #258
 featureLen = bytearray().fromhex("0000")
 features = bytearray()
 scid = bytearray([0, 0, 10, 0, 0, 2, 0, 1])   # for now we are placing it in the 10th block, 2nd transactions, 1st output
 satoshis = 11111
 
+#update fields
+timestamp = 1550513768     # timestamp is from time.time()
+timestamp = bytearray(timestamp.to_bytes(4, byteorder="big"))
+cltvDelta = 10
+cltvDelta = bytearray(cltvDelta.to_bytes(2, byteorder="big"))
+htlcMSat = 10000
+htlcMSat = bytearray(htlcMSat.to_bytes(8, byteorder="big"))
+feeBaseMSat = 1000
+feeBaseMSat = bytearray(feeBaseMSat.to_bytes(4, byteorder="big"))
+feePropMill = 1000
+feePropMill = bytearray(feePropMill.to_bytes(4, byteorder="big"))
+
+CHANNEL_ANNOUNCEMENT = "channel announcement"
+CHANNEL_UPDATE = "channel update"
+
+
 def mvp():
+    utility.setRandSeed(0)
     SelectParams("regtest")
     network = loadNetwork(networkSaveFile)
     ch = network.channels[0]
@@ -58,7 +78,6 @@ def mvp():
 
     h = a.hashPartial()
     s = sign(node1.cPrivObj, h)
-    print(len(s))
     nodesig1 = bytearray(sign(node1.cPrivObj, h))
     nodesig2 = bytearray(sign(node2.cPrivObj, h))
     bitcoinSig1 = bytearray(sign(cBitcoinPrivObj1, h))
@@ -71,23 +90,36 @@ def mvp():
 
     finalA = a.serialize(full=True)
 
-    myA = writeToGossip_Store(finalA, "gossip_store")
-    return myA
+    #channel updates
+    update1 = ChannelUpdate()
+    update1.setscid(scid)
+    update1.setTimestamp(timestamp)
+    mFlags = bytearray().fromhex("00")   #most sig bit is 0 because direction is from node 1->2. least sig bit is 0 because this is node 1's update
+    update1.setmFlags(mFlags)
+    cFlags = bytearray().fromhex("00")   #least sig bit this is node 1's update
+    update1.setcFlags(cFlags)
+    update1.setcltv(cltvDelta)
+    update1.sethtlcMSat(htlcMSat)
+    update1.setFeeBaseMSat(feeBaseMSat)
+    update1.setFeePropMill(feePropMill)
 
+    #update for node 1
+    s1 = sign(node1.cPrivObj, update1.hashPartial())
+    update1.setSig(s1)
+    bUpdate1 = update1.serialize(full=True)
 
-_bchr = chr
-_bord = ord
-if sys.version > '3':
-    long = int
-    _bchr = lambda x: bytes([x])
-    _bord = lambda x: x
+    update2 = update1
+    mFlags = bytearray().fromhex("01")  # most sig bit is 0 because direction is from node 1->2. Least sig bit is 1 because this is 2's update
+    update2.setmFlags(mFlags)
+    cFlags = bytearray().fromhex("01")  # least sig bit is 1 because this is 2's update
+    update2.setcFlags(cFlags)
+    s2 = sign(node2.cPrivObj, update2.hashPartial())
+    update2.setSig(s2)
+    bupdate2 = update2.serialize(full=True)
+    return finalA, bUpdate1, bupdate2
 
 def sign(key, h):
     sig, i = key.sign_compact(h)
-    # we don't need this because this extra byte if for key recovery
-    # meta = 27 + i
-    # if key.is_compressed:
-    #     meta += 4
     return sig
 
 def makeAllPrivPubKeys(network):
@@ -111,6 +143,48 @@ def compPubKey(node):
     return pubbits
 
 
+
+
+
+class ChannelUpdate():
+    def __init__(self):
+        self.HTLCMaxMSat = bytearray()  #since is it optional it starts out as empty
+    def setSig(self, sig):
+        self.sig = sig
+    def setscid(self, scid):
+        self.scid = scid
+    def setTimestamp(self,t):
+        self.timestamp = t
+    def setmFlags(self,f):
+        self.mFlags = f
+    def setcFlags(self, f):
+        self.cFlags = f
+    def setcltv(self, cltv):
+        self.cltv = cltv
+    def sethtlcMSat(self, msat):
+        self.HTLCMSat = msat
+    def setFeeBaseMSat(self, msat):
+        self.feeBaseMSat = msat
+    def setFeePropMill(self, fee):
+        self.feePropMill = fee
+    def setHTLCMaxMSat(self, msat):    #optional
+        self.HTLCMaxMSat = msat
+
+    def serialize(self, full):
+        if not full:
+            return chainHash + self.scid + self.timestamp + self.mFlags + \
+                   self.cFlags + self.cltv + self.HTLCMSat + self.feeBaseMSat + \
+                   self.feePropMill + self.HTLCMaxMSat
+        else:
+            return updateType + self.sig + chainHash + self.scid + self.timestamp + self.mFlags + \
+                   self.cFlags + self.cltv + self.HTLCMSat + self.feeBaseMSat + \
+                   self.feePropMill + self.HTLCMaxMSat
+    def hashPartial(self):
+        # we take the double sha
+        p = self.serialize(False)
+        h = hashlib.sha256(p).digest()
+        hh = hashlib.sha256(h).digest()
+        return hh
 
 class ChannelAnnouncement():
     def setNodeSig1(self, sig):
@@ -138,10 +212,10 @@ class ChannelAnnouncement():
 
     def serialize(self, full):
         if not full:
-            a = announcementType + self.featureLen + self.features + chainHash + self.scid + \
+            a = self.featureLen + self.features + chainHash + self.scid + \
                 self.id1 + self.id2 + self.bitcoinKey1 + self.bitcoinKey2
         else:
-            a = announcementType + self.sig1 + self.sig2 + self.bitcoinSig1 + \
+            a = channelType + self.sig1 + self.sig2 + self.bitcoinSig1 + \
                 self.bitcoinSig2 + self.featureLen + self.features + chainHash + self.scid + \
                 self.id1 + self.id2 + self.bitcoinKey1 + self.bitcoinKey2
         return a
@@ -153,7 +227,21 @@ class ChannelAnnouncement():
         """
         a = self.serialize(False)
         h = hashlib.sha256(a).digest()
-        return h
+        hh = hashlib.sha256(h).digest()
+        print("hash", hh.hex())
+        self.printAnnoucement(False)
+        return hh
+
+    def printAnnoucement(self, full):
+        if not full:
+            print("len:", self.featureLen.hex())
+            print("features", self.features.hex())
+            print("chain hash",chainHash.hex())
+            print("scid", self.scid.hex())
+            print("id1:", self.id1.hex())
+            print("id2:", self.id2.hex())
+            print("bitcoinKey1", self.bitcoinKey1.hex())
+            print("bitcoinKey2", self.bitcoinKey2.hex())
 
 
 def generateNewSecretKey():
@@ -172,31 +260,50 @@ def generateNewSecretKey():
 
 ## gossip store functions
 
-def writeToGossip_Store(a, filename):
+
+def writeToGossip_store(writeList, filename):
+    fp = open(filename, "wb")
+    initGossip_store(fp)
+    for entry in writeList:
+        a = entry[0]
+        t = entry[1]
+        if t == CHANNEL_ANNOUNCEMENT:
+            writeChannelAnnouncement(a, fp)
+        elif t == CHANNEL_UPDATE:
+            writeChannelUpdate(a, fp)
+
+
+def initGossip_store(fp):
     gossipVersion = bytearray().fromhex("03")
+    fp.write(gossipVersion)
+
+def writeChannelAnnouncement(a, fp):
     msglen = getmsglen(a)
     bMsglenA = bytearray(msglen.to_bytes(2, byteorder="big"))  # big endian because this is how gossip store loads it
     WIRE_GOSSIP_STORE_CHANNEL_ANNOUNCEMENT = bytearray().fromhex("1000")
     bSatoshis = bytearray(satoshis.to_bytes(8, byteorder="big"))
     fulllen = len(WIRE_GOSSIP_STORE_CHANNEL_ANNOUNCEMENT) + len(bMsglenA) + len(a) + len(bSatoshis) #remember, we don't have checksum and we don't count gossipVersion
-    print(fulllen)
     bMsglenFull = bytearray(fulllen.to_bytes(4, byteorder="big"))
-    aToWrite = gossipVersion + bMsglenFull  + WIRE_GOSSIP_STORE_CHANNEL_ANNOUNCEMENT + bMsglenA + a + bSatoshis
-    fp = open(filename, "wb")
+    aToWrite = bMsglenFull + WIRE_GOSSIP_STORE_CHANNEL_ANNOUNCEMENT + bMsglenA + a + bSatoshis
     fp.write(aToWrite)
-    print("len of a:", len(a))
-    print(len(aToWrite.hex()))
-    print(aToWrite.hex())
+    return aToWrite
+
+def writeChannelUpdate(a, fp):
+    msglen = getmsglen(a)
+    bMsglenA = bytearray(msglen.to_bytes(2, byteorder="big"))  # big endian because this is how gossip store loads it
+    WIRE_GOSSIP_STORE_CHANNEL_UPDATE = bytearray().fromhex("1001")
+    fulllen = len(WIRE_GOSSIP_STORE_CHANNEL_UPDATE) + len(bMsglenA) + len(a) #remember, we don't have checksum and we don't count gossipVersion
+    bMsglenFull = bytearray(fulllen.to_bytes(4, byteorder="big"))
+    aToWrite = bMsglenFull + WIRE_GOSSIP_STORE_CHANNEL_UPDATE + bMsglenA + a
+    fp.write(aToWrite)
+    # print("len of u:", len(a))
+    # print(len(aToWrite.hex()))
+    # print(aToWrite.hex())
     return aToWrite
 
 
 def getmsglen(a):
     return len(a)
-
-
-
-
-
 
 
 def loadNetwork(networkFilename):
@@ -206,14 +313,24 @@ def loadNetwork(networkFilename):
 
 
 
-ca = "03" + "000001bc" + "1000" + "01b00100bb8d7b6998cca3c2b3ce12a6bd73a8872c808bb48de2a30c5ad9cdf835905d1e27505755087e675fb517bbac6beb227629b694ea68f49d357458327138978ebfd7adfde1c69d0d2f497154256f6d5567a5cf2317c589e0046c0cc2b3e986cf9b6d3b44742bd57bce32d72cd1180a7f657795976130b20508b239976d3d4cdc4d0d6e6fbb9ab6471f664a662972e406f519eab8bce87a8c0365646df5acbc04c91540b4c7c518cec680a4a6af14dae1aca0fd5525220f7f0e96fcd2adef3c803ac9427fe71034b55a50536638820ef21903d09ccddd38396675b598587fa886ca711415c813fc6d69f46552b9a0a539c18f265debd0e2e286980a118ba349c216000043497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea33090000000013a63c0000b50001021bf3de4e84e3d52f9a3e36fbdcd2c4e8dbf203b9ce4fc07c2f03be6c21d0c67503f113414ebdc6c1fb0f33c99cd5a1d09dd79e7fdf2468cf1fe1af6674361695d203801fd8ab98032f11cc9e4916dd940417082727077609d5c7f8cc6e9a3ad25dd102517164b97ab46cee3826160841a36c46a2b7b9c74da37bdc070ed41ba172033a0000000001000000"
-
-with open(os.path.join("/home/jnetti/MCP/million-channels-project/test/", 'gossip_store'), 'wb') as f:
-    f.write(bytearray.fromhex("03"  # GOSSIP_VERSION
-                              "000001bc" #"000001bc"  # len
-                              #"521ef598"  # csum
-                              "1000"  # WIRE_GOSSIP_STORE_CHANNEL_ANNOUNCEMENT
-                              "01b00100bb8d7b6998cca3c2b3ce12a6bd73a8872c808bb48de2a30c5ad9cdf835905d1e27505755087e675fb517bbac6beb227629b694ea68f49d357458327138978ebfd7adfde1c69d0d2f497154256f6d5567a5cf2317c589e0046c0cc2b3e986cf9b6d3b44742bd57bce32d72cd1180a7f657795976130b20508b239976d3d4cdc4d0d6e6fbb9ab6471f664a662972e406f519eab8bce87a8c0365646df5acbc04c91540b4c7c518cec680a4a6af14dae1aca0fd5525220f7f0e96fcd2adef3c803ac9427fe71034b55a50536638820ef21903d09ccddd38396675b598587fa886ca711415c813fc6d69f46552b9a0a539c18f265debd0e2e286980a118ba349c216000043497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea33090000000013a63c0000b50001021bf3de4e84e3d52f9a3e36fbdcd2c4e8dbf203b9ce4fc07c2f03be6c21d0c67503f113414ebdc6c1fb0f33c99cd5a1d09dd79e7fdf2468cf1fe1af6674361695d203801fd8ab98032f11cc9e4916dd940417082727077609d5c7f8cc6e9a3ad25dd102517164b97ab46cee3826160841a36c46a2b7b9c74da37bdc070ed41ba172033a0000000001000000"))
+#ca = "03" + "000001bc" + "1000" + "01b00100bb8d7b6998cca3c2b3ce12a6bd73a8872c808bb48de2a30c5ad9cdf835905d1e27505755087e675fb517bbac6beb227629b694ea68f49d357458327138978ebfd7adfde1c69d0d2f497154256f6d5567a5cf2317c589e0046c0cc2b3e986cf9b6d3b44742bd57bce32d72cd1180a7f657795976130b20508b239976d3d4cdc4d0d6e6fbb9ab6471f664a662972e406f519eab8bce87a8c0365646df5acbc04c91540b4c7c518cec680a4a6af14dae1aca0fd5525220f7f0e96fcd2adef3c803ac9427fe71034b55a50536638820ef21903d09ccddd38396675b598587fa886ca711415c813fc6d69f46552b9a0a539c18f265debd0e2e286980a118ba349c216000043497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea33090000000013a63c0000b50001021bf3de4e84e3d52f9a3e36fbdcd2c4e8dbf203b9ce4fc07c2f03be6c21d0c67503f113414ebdc6c1fb0f33c99cd5a1d09dd79e7fdf2468cf1fe1af6674361695d203801fd8ab98032f11cc9e4916dd940417082727077609d5c7f8cc6e9a3ad25dd102517164b97ab46cee3826160841a36c46a2b7b9c74da37bdc070ed41ba172033a0000000001000000"
+update =  "00000086" + "88c703c8"  +"1001" + "008201021ea7c2eadf8a29eb8690511a519b5656e29aa0a853771c4e38e65c5abf43d907295a915e69e451f4c7a0c3dc13dd943cfbe3ae88c0b96667cd7d58955dbfedcf43497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea33090000000013a63c0000b500015b8d9b440000009000000000000003e8000003e800000001"
+# print(update)
+# with open(os.path.join(l1.daemon.lightning_dir, 'gossip_store'), 'wb') as f:
+#     f.write(bytearray.fromhex("03"  # GOSSIP_VERSION
+#                               "000001bc"  # len
+#                               "521ef598"  # csum
+#                               "1000"  # WIRE_GOSSIP_STORE_CHANNEL_ANNOUNCEMENT
+#                               "01b00100bb8d7b6998cca3c2b3ce12a6bd73a8872c808bb48de2a30c5ad9cdf835905d1e27505755087e675fb517bbac6beb227629b694ea68f49d357458327138978ebfd7adfde1c69d0d2f497154256f6d5567a5cf2317c589e0046c0cc2b3e986cf9b6d3b44742bd57bce32d72cd1180a7f657795976130b20508b239976d3d4cdc4d0d6e6fbb9ab6471f664a662972e406f519eab8bce87a8c0365646df5acbc04c91540b4c7c518cec680a4a6af14dae1aca0fd5525220f7f0e96fcd2adef3c803ac9427fe71034b55a50536638820ef21903d09ccddd38396675b598587fa886ca711415c813fc6d69f46552b9a0a539c18f265debd0e2e286980a118ba349c216000043497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea33090000000013a63c0000b50001021bf3de4e84e3d52f9a3e36fbdcd2c4e8dbf203b9ce4fc07c2f03be6c21d0c67503f113414ebdc6c1fb0f33c99cd5a1d09dd79e7fdf2468cf1fe1af6674361695d203801fd8ab98032f11cc9e4916dd940417082727077609d5c7f8cc6e9a3ad25dd102517164b97ab46cee3826160841a36c46a2b7b9c74da37bdc070ed41ba172033a0000000001000000"
+#                               "00000086"  # len
+#                               "88c703c8"  # csum
+#                               "1001"  # WIRE_GOSSIP_STORE_CHANNEL_UPDATE
+#                               "008201021ea7c2eadf8a29eb8690511a519b5656e29aa0a853771c4e38e65c5abf43d907295a915e69e451f4c7a0c3dc13dd943cfbe3ae88c0b96667cd7d58955dbfedcf43497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea33090000000013a63c0000b500015b8d9b440000009000000000000003e8000003e800000001"
+#                               "00000099"  # len
+#                               "12abbbba"  # csum
+#                               "1002"  # WIRE_GOSSIP_STORE_NODE_ANNOUNCEMENT
+#                               "00950101cf5d870bc7ecabcb7cd16898ef66891e5f0c6c5851bd85b670f03d325bc44d7544d367cd852e18ec03f7f4ff369b06860a3b12b07b29f36fb318ca11348bf8ec00005aab817c03f113414ebdc6c1fb0f33c99cd5a1d09dd79e7fdf2468cf1fe1af6674361695d23974b250757a7a6c6549544300000000000000000000000000000000000000000000000007010566933e2607"))
+#
 
 
 
@@ -222,7 +339,28 @@ def main():
     makeAllPrivPubKeys(network) # TODO: eventually there should be a check for cmd arg to see where privkeys were already calculated
 
 
-myA = mvp()
-diff = len(ca) - len(myA.hex())
-i = len(ca) - diff
-print(ca[i:])
+myA, update1, update2 = mvp()
+h = myA.hex()[516:]
+# for i in range(0, len(h), 2):
+#     print(h[i]+h[i+1])
+print(update1.hex()[220:])
+# saved = "01021ea7c2eadf8a29eb8690511a519b5656e29aa0a853771c4e38e65c5abf43d907295a915e69e451f4c7a0c3dc13dd943cfbe3ae88c0b96667cd7d58955dbfedcf43497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea33090000000013a63c0000b500015b8d9b440000009000000000000003e8000003e800000001"
+# print(saved[219:])
+# lst = [(myA, CHANNEL_ANNOUNCEMENT), (update1, CHANNEL_UPDATE), (update2, CHANNEL_UPDATE)]
+# writeToGossip_store(lst, "gossip_store")
+#
+#
+
+print("hashTests")
+h = hashlib.sha3_256(bytearray().fromhex("00")).digest()
+hh = hashlib.sha3_256(h).digest()
+print(hh.hex())
+h = hashlib.sha256(bytearray().fromhex("00")).digest()
+hh = hashlib.sha256(h).digest()
+print(hh.hex())
+h = hashlib.sha3_256(bytearray().fromhex("00000000")).digest()
+hh = hashlib.sha3_256(h).digest()
+print(hh.hex())
+h = hashlib.sha256(bytearray().fromhex("00000000")).digest()
+hh = hashlib.sha256(h).digest()
+print(hh.hex())
